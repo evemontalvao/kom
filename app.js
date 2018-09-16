5dfac38c78ef62b1cb450709e3958052045b9494
Eve Montalvão <evemontalvao@gmail.com>
2018-09-15 21:22:37 -0300

    add server

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const routes = require('./routes.json');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/assets', express.static(__dirname + '/assets'));
+
+app.use('/', (req, res) => {
+  const file = routes[req.path].file
+
+  res.sendFile(path.resolve(`./_site/${file}`))
+});
+
+app.listen('2107', () => {
+  console.log('Server listening to 2107')
+})
